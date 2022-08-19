9587067b6070f417466abab097a294743b63033c
Lukas <lg2711@hotmail.de>
2022-08-19 11:16:26 +0200

    Added alternative format for embeddings

--- Code/Process.py
+++ Code/Process.py
@@ -29,7 +29,7 @@ def prepareFromNPY(filepathIn=None):
                 titles = []
                 abstracts = []
                 for line in doc:
-                        pmids.append(np.ndarray.tolist(line[0]))
+                        pmids.append(np.ndarray.tolist((int)(line[0])))
                         titles.append(np.ndarray.tolist(line[1]))
                         abstracts.append(np.ndarray.tolist(line[2]))
                 return (pmids, titles, abstracts)
@@ -66,7 +66,7 @@ def generateWord2VecModel(abstractsRELISH, titlesRELISH, filepathOut):
                 model = Word2Vec(sentences=sentenceList, vector_size=200, epochs=5, window=5, min_count=1, workers=4)
                 model.save(filepathOut)
 
-def generateDocumentEmbeddings(pmids=None, titles=None, abstracts=None, directoryOut=None, gensimModelPath=None):
+def generateDocumentEmbeddings(pmids=None, titles=None, abstracts=None, directoryOut=None, gensimModelPath=None, saveAs="numpy"):
         '''
         Generates document embeddings from a titles and abstracts in a given paper using word2vec and calculating the cenroids of all given word embeddings.
         If no gensim model is given, the 'glove-wiki-gigaword-200' gensim model is used.
@@ -83,6 +83,8 @@ def generateDocumentEmbeddings(pmids=None, titles=None, abstracts=None, director
                 The filepath of the output directory of all .npy embeddings.
         gensimModelPath: str (optional)
                 The filepath of the custom gensimModel.
+        saveAs: str
+                Can be saved as either 'numpy' or 'pandas'.
         '''
         if not isinstance(pmids, list):
                 logging.alert("Wrong parameter type for generateDocumentEmbeddings.")
@@ -96,8 +98,10 @@ def generateDocumentEmbeddings(pmids=None, titles=None, abstracts=None, director
         elif not isinstance(directoryOut, str):
                 logging.alert("Wrong parameter type for generateDocumentEmbeddings.")
                 sys.exit("directoryOut needs to be of type string")
+        elif not isinstance(saveAs, str) or not (saveAs == "numpy" or saveAs == "pandas"):
+                logging.alert("Wrong parameter type or value for saveAs.")
+                sys.exit("saveAs needs to be a string of either 'numpy' or 'pandas'")
         else:
-                import numpy as np
                 import gensim.downloader as api
                 import gensim.models as model
                 word_vectors = None
@@ -149,13 +153,20 @@ def generateDocumentEmbeddings(pmids=None, titles=None, abstracts=None, director
                         documentEmbeddings.append(document)
                         iteration += 1
                 iteration = 0
-                while(iteration < len(documentEmbeddings)):
-                        np.save(f'{directoryOut}/{pmids[iteration]}', documentEmbeddings[iteration])
-                        iteration += 1
+                if(saveAs == 'pandas'):
+                        import pandas as pd
+                        df = pd.DataFrame(list(zip((pmids), documentEmbeddings)), columns =['pmids', 'embeddings'])
+                        df = df.sort_values('pmids')
+                        df.to_pickle(f'{directoryOut}/embeddings.pkl')
+                elif(saveAs == 'numpy'):
+                        import numpy as np
+                        while(iteration < len(documentEmbeddings)):
+                                np.save(f'{directoryOut}/{pmids[iteration]}', documentEmbeddings[iteration])
+                                iteration += 1
 
 def addCosineSimilarity(EvaluationFile, EmbeddingsDirectory):
         '''
-        Adds cosine similarity to the evaluation matrix.
+        Adds cosine similarity to the evaluation matrix from the .npy formatted embeddings.
 
         Parameters
         ----------
